7d480543f76dbd0fa387ab94dd0b20f5c9552908
Eduardo Vargas <evargas9@gatech.edu>
2018-11-14 21:32:02 -0500

    update css + youtube js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
+
+
 jQuery.noConflict();
 (function($) {
   $(function() {
+    document.getElementById('demo').style.display = "none";
+    document.getElementById('codeLabel').style.display = "none";
+    document.getElementById('codePanel').style.display = "none";
+
     $('button').click(function(e) {
       var elements = document.getElementById('input_form').elements;
+      document.getElementById('demo').style.display = "";
+      document.getElementById('codeLabel').style.display = "";
+      document.getElementById('codePanel').style.display = "";
       var url;
       var mediaType;
       var alignment;
@@ -22,40 +31,20 @@ jQuery.noConflict();
         }
       }
       document.getElementById('codeLabel').innerText = 'Code: ';
+    
 
       if (this.mediaType === 'video') {
-        document.getElementById('demo').innerHTML ='Preview: <div style="overflow:hidden;padding-bottom:56.25%;position:relative;height:0;width=' +this.width +'%"> \n  <iframe \n style="left:0;top:0;height:100%;width:100%;position:absolute;" \n  width="420" \n height="315" \n src="' +this.url +'" \n frameborder="0" \n allowfullscreen> \n </iframe> \n </div> ';
+        this.url = this.url.split("v=").pop();
+        this.url = this.url.split("&")[0];
+        document.getElementById('demo').innerHTML ='Preview: <div style="overflow:hidden;padding-bottom:56.25%;position:relative;height:0;width=' +this.width +'%"> \n  <iframe \n style="left:0;top:0;height:100%;width:100%;position:absolute;" \n  width="420" \n height="315" \n src="https://www.youtube.com/embed/' + this.url +'" \n frameborder="0" \n allowfullscreen> \n </iframe> \n </div> ';
 
         document.getElementById('code').innerText =
-          '<div style="overflow:hidden;padding-bottom:56.25%;position:relative;float:' +this.alignment + ';height:0;width=' + this.width + '%"> \n \n<iframe \n style="left:0;top:0;height:100%;width:100%;position:absolute;" \n  width="420" \n height="315" \n src="' + this.url + '" \n frameborder="0" \n allowfullscreen>\n </iframe> \n </div> ';
-      } else if (this.mediaType === 'image') {
+          '<div style="overflow:hidden;\npadding-bottom:56.25%;\nposition:relative;\nfloat:' + this.alignment + ';\nheight:0;\nwidth=' + this.width + '%"> \n \n<iframe \n style="left:0;\ntop:0;\nheight:100%;\nwidth:100%;\nposition:absolute;" \n  width="420" \n height="315" \n src="https://www.youtube.com/embed/' + this.url + '" \n frameborder="0" \n allowfullscreen>\n </iframe> \n </div> ';
+      
+        } else if (this.mediaType === 'image') {
         document.getElementById('demo').innerHTML = 'Preview: <img src="' + this.url + '" alt="image" style="float:' + this.alignment + ';width=' + this.width + '%" />';
         document.getElementById('code').innerText = '<img src="' + this.url + '" alt="image" style="float:' + this.alignment + ';width=' + this.width + '%" />';
       }
     });
   });
 })(jQuery);
-
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-83886479-2']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script');
-  ga.type = 'text/javascript';
-  ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(ga, s);
-})();
-
-function trackButtonClick(e) {
-  _gaq.push(['_trackEvent', e.target.id, 'clicked']);
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-  var buttons = document.querySelectorAll('button');
-  for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', trackButtonClick);
-  }
-});
